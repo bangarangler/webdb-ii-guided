680f55c9aac1add921bb96a03e0403b7eaf38b8d
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-26 14:06:39 -0400

    Completed Guided Project

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,28 +1,54 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const knex = require("knex");
 
-router.get('/', (req, res) => {
+const knexConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./Data/Roles.db3"
+  }
+};
+
+const db = knex(knexConfig);
+
+router.get("/", (req, res) => {
   // get the roles from the database
-  res.send('Write code to retrieve all roles');
+  db("roles")
+    .then(roles => {
+      res.status(200).json(roles);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
+  const { id } = req.params;
+  db("roles")
+    .where({ id: id })
+    .first()
+    .then(role => {
+      res.status(200).json(role);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // add a role to the database
-  res.send('Write code to add a role');
+  res.send("Write code to add a role");
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update roles
-  res.send('Write code to modify a role');
+  res.send("Write code to modify a role");
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // remove roles (inactivate the role)
-  res.send('Write code to remove a role');
+  res.send("Write code to remove a role");
 });
 
 module.exports = router;
